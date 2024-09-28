afb21addfe057f3ba98cd038cc5d89a3aba04247
Md Mohin Uddin <mdmohinuddin.cse@gmail.com>
2024-09-28 22:35:31 +0600

    doctors experience section

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { formatDate } from "../../utils/formateDate";
 
 const DoctorAbout = () => {
   return (
@@ -27,7 +28,7 @@ const DoctorAbout = () => {
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                23 June, 2008
+                {formatDate("1-08-2010")}
               </span>
               <p className="text-[16px] leading-6 font-medium text-textColor">
                 PhD in Surgery
@@ -42,12 +43,41 @@ const DoctorAbout = () => {
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                23 June, 2008
+                {formatDate("10-10-2012")}
               </span>
               <p className="text-[16px] leading-6 font-medium text-textColor">
                 PhD in Surgery
               </p>
             </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Experience
+        </h3>
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formatDate("08-13-2014")} - {formatDate("07-04-2010")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formatDate("08-13-2014")} - {formatDate("07-04-2010")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
             <p className="text-[14px] leading-5 font-medium text-textColor">
               New Apollo Hospital, New York.
             </p>
