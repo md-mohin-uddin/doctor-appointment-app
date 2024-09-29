a8233f94bb667f70e4f8ca6eb88850e311b7ea7d
Md Mohin Uddin <mdmohinuddin.cse@gmail.com>
2024-09-30 00:21:03 +0600

    update on footer

--- frontend/src/components/Services/ServicesCard.jsx
+++ frontend/src/components/Services/ServicesCard.jsx
@@ -15,7 +15,7 @@ const ServicesCard = ({ item, index }) => {
       </p>
       <div className="flex items-center justify-between mt-[30px]">
         <Link
-          to="/doctors"
+          to="/services"
           className="w-[44px] h-[44px] leading-full border rounded-full border-solid border-[#181A1E]  flex items-center justify-center group hover:bg-primaryColor hover:border-none"
         >
           <BsArrowRight className="group-hover:text-white w-6 h-5" />
--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -52,7 +52,7 @@ const quickLinks01 = [
 ];
 const quickLinks02 = [
   {
-    path: "/find-a-doctor",
+    path: "/doctors",
     display: "Find a Doctor",
   },
   {
